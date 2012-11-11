ae27381d9cbafaae5a9e775d465c4ba72ce9e8f4
login73ul <login73ul@gmail.com>
2012-11-11 16:30:44 +0400

    rails new lab --database=postgres # lab/config/database.yml redacted developers rake db:create # look pgAdmin and see new database #use scaffold rails g scaffold news title:string author:string article:text # use rake -T for print list rake rake db:migrate rake db:schema:load

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lab::Application.config.secret_token = '6f62e664c76bf97a728e3de42acd8d6ab6754ef209cf1a24b735fa88b7363967f03db5687d7adc5856cf4bb7e0f3291ccecd20b57a7e34e403345a91fd64f612'
---
+++ db/migrate/20121111120211_create_news.rb
@@ -0,0 +1,11 @@
+class CreateNews < ActiveRecord::Migration
+  def change
+    create_table :news do |t|
+      t.string :title
+      t.string :author
+      t.text :article
+
+      t.timestamps
+    end
+  end
+end
